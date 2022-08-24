aab71c7e1e87a7039cc0316ce2a16079b553c13b
eunsol-an <dev.eunsol@gmail.com>
2022-08-25 03:09:26 +0900

    Create encapsulation.java
    learn encapsulation

---
+++ _core/05_Class/encapsulation/encapsulation.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class encapsulation {
+
+	public static void main(String[] args) {
+		ColdPatient sufferer = new ColdPatient();
+		Contac600 contac600 = new Contac600();
+		sufferer.take(contac600);
+	}
+
+}
