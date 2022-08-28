d1029a7e71a63f78e4d342afcb21f3070faa7b7c
eunsol-an <dev.eunsol@gmail.com>
2022-08-29 00:58:56 +0900

    Create KimchiRef.java
    learn abstracts

---
+++ _core/07_Inheritance/abstracts/KimchiRef.java
@@ -0,0 +1,40 @@
+package abstracts;
+
+public abstract class KimchiRef extends Cooler {
+
+	@Override
+	public boolean isPower() {
+		// TODO Auto-generated method stub
+		return super.isPower();
+	}
+
+	@Override
+	public void setPower(boolean power) {
+		super.setPower(power);
+	}
+
+	@Override
+	public int getButton() {
+		// TODO Auto-generated method stub
+		return super.getButton();
+	}
+
+	@Override
+	public void setButton(int button) {
+		System.out.println("발효 버튼 추가");
+		super.setButton(button);
+	}
+
+	@Override
+	public String getFeature() {
+		// TODO Auto-generated method stub
+		return super.getFeature();
+	}
+
+	@Override
+	public void setFeature(String feature) {
+		System.out.println("발효 과학 추가");
+		super.setFeature(feature);
+	}
+	
+}
