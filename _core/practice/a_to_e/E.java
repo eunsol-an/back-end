bfd49cedeb21d40179c0e273cdfee96b14ca2e9f
eunsol-an <dev.eunsol@gmail.com>
2022-09-24 01:06:49 +0900

    Create E.java
    practice a to e

---
+++ _core/practice/a_to_e/E.java
@@ -0,0 +1,17 @@
+package a_to_e;
+
+public class E {
+	int x = 50;
+	
+	public E() {
+		System.out.println("E를 거쳤습니다");
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+}
